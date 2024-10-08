068b622e64fa009e4913ee1f3c942f9999af2f1c
Aviva Ruben <aviva@rubenfamily.com>
2024-10-07 21:40:08 -0500

    lowering pt3/2

--- src/lower.rs
+++ src/lower.rs
@@ -10,7 +10,7 @@ pub enum ListAmount {
 }
 
 /// Lowered Abstract Syntax Tree
-pub enum LAST<'s> {
+pub enum LASTNode<'s> {
     String(&'s str),
     Directive(&'s str),
     Choice {
@@ -24,6 +24,7 @@ pub enum LAST<'s> {
         then: Box<LAST<'s>>,
     },
 }
+pub type LAST<'s> = (LASTNode<'s>, Option<&'s str>);
 
 pub fn lower<'s>(stmts: Vec<Stmt<'s>>) {
     let mut stmts = stmts;
@@ -44,8 +45,23 @@ pub fn lower<'s>(stmts: Vec<Stmt<'s>>) {
     let ast = lower_ast(entrypoint, &RefCell::new(idents));
 }
 
-fn lower_ast<'s>(expr: Expr<'s>, idents: &RefCell<FxHashMap<&str, Expr<'s>>>) -> Expr<'s> {
+fn lower_ast<'s>(expr: Expr<'s>, idents: &RefCell<FxHashMap<&str, Expr<'s>>>) -> LAST<'s> {
     match expr {
+        Expr::String(s) => (LASTNode::String(s), None),
+        Expr::Directive(d) => (LASTNode::Directive(d), None),
+        Expr::Described(expr, description) => {
+            let l_ast = lower_ast(*expr, idents).0;
+            let description = lower_ast(*description, idents);
+
+            if let LASTNode::String(desc_str) = description.0 {
+                if description.1.is_some() {
+                    panic!("description can't have a description")
+                }
+                return (l_ast, Some(desc_str));
+            } else {
+                panic!("description must reduce to a string")
+            }
+        }
         Expr::Ident(ident) => {
             // TODO: replace expect with proper syntax errors
             // TODO: avoid clones with copy on write
@@ -54,28 +70,39 @@ fn lower_ast<'s>(expr: Expr<'s>, idents: &RefCell<FxHashMap<&str, Expr<'s>>>) ->
                 idents
                     .borrow()
                     .get(ident)
+                    // TODO: check for cycle! non turing complete and immutable so any recurrence is a cycle
+                    // TODO: replace ident with value as optimization? need to handle differing context
                     .expect("ident is defined")
                     .clone(),
                 idents,
             )
         }
         Expr::Infix(lhs, infix, rhs) => {
-            match (lower_ast(*lhs, idents), infix, lower_ast(*rhs, idents)) {
+            match (*lhs, infix, *rhs) {
                 (Expr::List(mut lhs), InfixSymbol::Concat, Expr::List(rhs)) => {
                     lhs.extend(rhs);
-                    Expr::List(lhs)
+                    lower_ast(Expr::List(lhs), idents)
                 }
+
                 (Expr::List(lhs), InfixSymbol::SetMinus, Expr::List(rhs)) => {
                     let rhs = rhs.into_iter().collect::<FxHashSet<Expr<'_>>>();
-                    Expr::List(lhs.into_iter().filter(|e| rhs.contains(e)).collect())
+                    lower_ast(
+                        Expr::List(lhs.into_iter().filter(|e| rhs.contains(e)).collect()),
+                        idents,
+                    )
                 }
                 (_lhs, InfixSymbol::Concat | InfixSymbol::SetMinus, _rhs) => {
                     // TODO: use proper spanned error, report type ect
                     panic!("list infix operators should only be used on lists")
                 }
-                (lhs, infix, rhs) => Expr::Infix(Box::new(lhs), infix, Box::new(rhs)),
+                (lhs, infix, rhs) => (
+                    LASTNode::Dependant {
+                        when: Box::new(lower_ast(lhs, idents)),
+                        then: Box::new(lower_ast(rhs, idents)),
+                    },
+                    None,
+                ),
             }
-        } // Expr::v => v,
-        v => v,
+        }
     }
 }
